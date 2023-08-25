db8dbc583876108969cbe4bdfb7c1fbe03f2ea25
MadokaAbe <132809038+madoka105@users.noreply.github.com>
2023-08-25 17:19:52 +0000

    fix_admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,14 @@
 class Admin::PostsController < ApplicationController
   def show
+    @post = Post.find(params[:id])
   end
 
   def index
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_root_path
   end
 end
