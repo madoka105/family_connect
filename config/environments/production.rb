ad1ec9fa1f2762c0183e280f9c7610ed25d23075
MadokaAbe <132809038+madoka105@users.noreply.github.com>
2023-08-29 11:11:48 +0000

    action_cable_fix5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   ActionCable.server.config.disable_request_forgery_protection = true
   config.action_cable.url = 'wss://54.248.86.119/cable'
-  config.action_cable.allowed_request_origins = [ 'http://54.248.86.119',/http://54.248.86.119.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://54.248.86.119' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
